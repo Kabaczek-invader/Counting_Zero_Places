74fe5d0be280d4d56e3a4859f63d8eda4064518f
Mikolaj Ceglewski <ceglewski123@gmail.com>
2021-09-21 19:57:43 +0200

    commit

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import math
 
-a = int(input("podaj a "))
-b = int(input("podaj b "))
-c = int(input("podaj c "))
+a = int(input("Podaj a: "))
+b = int(input("Podaj b: "))
+c = int(input("Podaj c: "))
 
 delta = (b * b) - (4 * a * c)
 
-print('delta wynosi:', delta)
+print('delta wynosi: ',delta)
+
+if delta > 0:
+    x1 = -b - math.sqrt(delta) / (2 * a)
+    x2 = -b + math.sqrt(delta) / (2 * a)
+
+    print("x1 = ", x1)
+    print("x2 = ", x2)
+
+else:
+    if delta == 0:
+        x = -b / (2 * a)
+        print("x = ", x)
+    else:
+        print("brak miejsc zerowych")
+
+
+
+
+
+
